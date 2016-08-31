98ebe09fd5bc42e6e1993349764de55698810f65
Matt Smollinger <matt.smollinger@mapzen.com>
2016-08-31 13:40:46 -0400

    Adds unit testing and ability to build dynamic framework version

---
+++ on_the_road/on_the_road.h
@@ -0,0 +1,20 @@
+//
+//  on_the_road.h
+//  on_the_road
+//
+//  Created by Matt Smollinger on 8/31/16.
+//  Copyright © 2016 Trailbehind inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for on_the_road.
+FOUNDATION_EXPORT double on_the_roadVersionNumber;
+
+//! Project version string for on_the_road.
+FOUNDATION_EXPORT const unsigned char on_the_roadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <on_the_road/PublicHeader.h>
+
+
+#import "OTRRouting.h"
