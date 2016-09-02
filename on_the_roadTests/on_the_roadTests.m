3e465a749bb81b0f6fd1eb306831ff77796c54df
Matt Smollinger <matt.smollinger@mapzen.com>
2016-09-02 12:31:23 -0400

    Includes necessary .m files for compilation in unit tests and adds basic unit test for assertion.

--- on_the_roadTests/on_the_roadTests.m
+++ on_the_roadTests/on_the_roadTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "OTRRouting.h"
 
 @interface on_the_roadTests : XCTestCase
 
@@ -24,16 +25,8 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testRoutingControllerInstantiation {
+  OTRRoutingController *controller = [[OTRRoutingController alloc] init];
+  XCTAssertNotNil(controller, "Controller is nil");
 }
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
